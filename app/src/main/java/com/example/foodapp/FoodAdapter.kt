8b70434d9a4d1a6d8fa7de0dce6948bfac6c56a0
Earbaj <earbajsaria3@gmail.com>
2022-07-23 11:31:21 -0700

    Add FoodAdapter class and implement getview methode and call it from mainactivity

---
+++ app/src/main/java/com/example/foodapp/FoodAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.foodapp
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import kotlinx.android.synthetic.main.food_design_layout.view.*
+
+class FoodAdapter: BaseAdapter {
+    var listOfFood= ArrayList<Food>()
+    var context:Context?=null
+    constructor(context: Context, listOfFood:ArrayList<Food>):super() {
+        this.context = context
+        this.listOfFood=listOfFood
+    }
+
+    override fun getCount(): Int {
+        return listOfFood.size
+    }
+
+    override fun getItem(p0: Int): Any {
+        return listOfFood[p0]
+    }
+
+    override fun getItemId(p0: Int): Long {
+        return p0.toLong()
+    }
+
+    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+        val food = this.listOfFood[p0]
+        var inflator = context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        var foodView= inflator.inflate(R.layout.food_design_layout,null)
+        foodView.imageView.setImageResource(food.img!!)
+        foodView.textView.text =  food.name!!
+        return  foodView
+    }
+}
